2e8ec81bac3fe839c515691886177c4da58f3343
lemon <lemon52121@163.com>
2017-08-01 16:54:48 +0800

    修改getCaptureMessage方法

--- HBase/src/test/java/com/hzgc/hbase/staticreposuite/CapturePictureTest.java
+++ HBase/src/test/java/com/hzgc/hbase/staticreposuite/CapturePictureTest.java
@@ -1,9 +1,10 @@
 package com.hzgc.hbase.staticreposuite;
 
 import com.hzgc.dubbo.dynamicrepo.CapturedPicture;
+import com.hzgc.dubbo.dynamicrepo.PictureType;
 import com.hzgc.hbase.dynamicrepo.CapturePictureSearchServiceImpl;
-import com.hzgc.hbase.util.HBaseHelper;
-import org.apache.hadoop.hbase.client.Table;
+import com.hzgc.hbase.dynamicrepo.DynamicPhotoServiceImpl;
+import org.junit.Test;
 
 /**
  * CapturePictureSearchServiceImpl test class
@@ -11,38 +12,24 @@ import org.apache.hadoop.hbase.client.Table;
 public class CapturePictureTest {
 
     //private static final String rowkey = "17130NCY0HZ0004-0_00000000000000_170523160015_0000004075_01";
-    private static final String rowkey = "62513NCY0HZ3544151_0000000000000_170523160015_0000004075_01";
+    //private static final String rowkey = "62513NCY0HZ3544151_0000000000000_170523160015_0000004075_01";
     private static final int type = 5;
+    private static final String rowkey = "62513NCY0HZ3544151_0000000000000_170523160015_0000004075_51";
+    private static final float[] Float = {1.175494351f, 0.125f};
 
-    @org.junit.Test
+    @Test
     public void getCaptureMessageTest() {
         CapturePictureSearchServiceImpl capturePictureSearchService = new CapturePictureSearchServiceImpl();
         CapturedPicture capturedPicture = new CapturedPicture();
         capturedPicture = capturePictureSearchService.getCaptureMessage(rowkey, type);
-        if (null != capturedPicture) {
-            if (null != capturedPicture.getBigImage()) {
-                System.out.println(capturedPicture.getBigImage());
-            }
-            if (null != capturedPicture.getDescription()) {
-                System.out.println(capturedPicture.getDescription());
-            }
-            if (null != capturedPicture.getExtend()) {
-
-                System.out.println(capturedPicture.getExtend());
-            }
-            if (null != capturedPicture.getId()) {
-                System.out.println(capturedPicture.getId());
-            }
-
-            System.out.println(capturedPicture.getSimilarity());
-
-            if (null != capturedPicture.getSmallImage()) {
-                System.out.println(capturedPicture.getSmallImage());
-            }
-
-            System.out.println(capturedPicture.getTimeStamp());
-        }
+        System.out.println(capturedPicture);
     }
 
+    @Test
+    public void insertePictureFeatureTest() {
+        DynamicPhotoServiceImpl dynamicPhotoService = new DynamicPhotoServiceImpl();
+        boolean b = dynamicPhotoService.insertePictureFeature(PictureType.CAR, rowkey, Float);
+        System.out.println(b);
+    }
 
 }
