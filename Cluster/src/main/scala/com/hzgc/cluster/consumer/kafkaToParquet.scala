12e0dfad283372c10f19d22eabc0286504346258
ck0903 <ck@hzgosun.com>
2017-11-02 16:19:33 +0800

    修改字段名称

--- Cluster/src/main/scala/com/hzgc/cluster/consumer/kafkaToParquet.scala
+++ Cluster/src/main/scala/com/hzgc/cluster/consumer/kafkaToParquet.scala
@@ -13,7 +13,7 @@ import org.apache.spark.streaming.{Duration, Durations, StreamingContext}
 
 object kafkaToParquet {
 
-  case class pic(picurl: String, eyeglasses: Int, turefeature: String, gender: Int, hairColor: Int, hairStyle: Int,
+  case class pic(ftpurl: String, eyeglasses: Int, turefeature: String, gender: Int, hairColor: Int, hairStyle: Int,
                  hat: Int, huzi: Int, tie: Int, ipcId: String, timequantum: String, timestamp: String, pictype: Int) {}
 
   val properties: Properties = StreamingUtils.getProperties
