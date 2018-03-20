bc271b81ee6b7d6eec8b68d9dd74ec988519094b
ck0903 <ck@hzgosun.com>
2018-03-20 17:21:09 +0800

    修改调试出现的多个设备无法返回问题的代码

--- Service/src/test/java/com/hzgc/service/dynamicrepo/CptureNumberImpTest1.java
+++ Service/src/test/java/com/hzgc/service/dynamicrepo/CptureNumberImpTest1.java
@@ -13,12 +13,12 @@ public class CptureNumberImpTest1 {
         SearchOption searchOption = new SearchOption();
         List<String> deviceIds = new ArrayList<>();
         //deviceIds.add("3B0383FPAG00883");
-        deviceIds.add("3K01E84PAU00150");
-        deviceIds.add("2L04129PAU01933");
-        deviceIds.add("DS-2CD2T20FD-I320160122AACH571485690");
+        //deviceIds.add("3K01E84PAU00150");
+        //deviceIds.add("2L04129PAU01933");
+        //deviceIds.add("DS-2CD2T20FD-I320160122AACH571485690");
         searchOption.setDeviceIds(deviceIds);
         List<SortParam> sortParams = new ArrayList<>();
-        sortParams.add(SortParam.IPC);
+       // sortParams.add(SortParam.IPC);
         sortParams.add(SortParam.TIMEDESC);
         searchOption.setSortParams(sortParams);
         searchOption.setStartDate("2018-03-12 00:00:00");
@@ -29,14 +29,12 @@ public class CptureNumberImpTest1 {
         List<SearchResult> list = capturePictureSearchService.getCaptureHistory(searchOption);
         for (SearchResult s : list) {
             List<SingleResult> singlelist = s.getResults();
-            System.out.println(singlelist.size() + "++++++");
             for (SingleResult ss : singlelist) {
                 List<CapturedPicture> capturedPictures = ss.getPictures();
                 for (CapturedPicture c : capturedPictures) {
                     String a = c.getSurl();
                     System.out.println(a);
                 }
-                System.out.println(capturedPictures.size());
             }
         }
     }
